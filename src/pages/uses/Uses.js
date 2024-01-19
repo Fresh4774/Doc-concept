43dbefab43b937aad8741082432e3d1c57d5f635
Friction <106335966+Fresh4774@users.noreply.github.com>
2024-01-20 03:50:38 +0530

    Aquin-V1.3

--- src/pages/uses/Uses.js
+++ src/pages/uses/Uses.js
@@ -27,7 +27,7 @@ export const Uses = () => {
         />
         <ProjectHeader
           title="AquinV1.3"
-          description="AquinV1.3 is has released on january 17 2024! it will a update to change and upgrade everything, it'll bring a tons of features to Aquin AI with full developed features and users can expect a complete enhanced UI/UX."
+          description="AquinV1.3 is has released on january 20 2024! it will a update to change and upgrade everything, it'll bring a tons of features to Aquin AI with full developed features and users can expect a complete enhanced UI/UX."
         />
         <ProjectSection padding="none" className={styles.section}>
           <ProjectSectionContent>
